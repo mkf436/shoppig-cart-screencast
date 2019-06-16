57cd90411b6f231a06821edf1395120430813d3b
Mari <mkf346@stern.nyu.edu>
2019-06-16 11:17:39 -0400

    Look up products given identifier

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,7 +2,6 @@
 
 # shopping_cart.py
 
-#from pprint import pprint
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -27,14 +26,11 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-
 #INFO CAPTURE
-
-
-selected_id = input("Please input a product indentifier: ")  
-print(selected_id)
-print(type(selected_id))
-
+selected_id = input("Please input a product indentifier: ")  # number shows as string in screen cast but mine is showing as int, why?
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
 
