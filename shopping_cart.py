6fc14f4e51cd20d2d94d6d915c720a73f4760f21
Mari <mkf346@stern.nyu.edu>
2019-06-16 12:19:23 -0400

    Checking off more requirements

--- shopping_cart.py
+++ shopping_cart.py
@@ -52,26 +52,21 @@ print("Phone: (1) 781-718-5136")
 import datetime
 currentDT = datetime.datetime.now()
 print("Check out Time: " + str(currentDT))
+print("--------------------")
 
 for selected_id in selected_ids:
     matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
     matching_product = matching_products[0]
     total_price = total_price + matching_product["price"]
-    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+    print("SELECTED PRODUCT: " + matching_product["name"] + " $" + str(matching_product["price"]))
 
-print("TOTAL PRICE: " + str(total_price))  #format as USD
+print("TOTAL PRICE: $" + str(total_price))  #format as USD
 
 #print(selected_ids)
 
 
 
 
-
-#A grocery store name of your choice
-#A grocery store phone number and/or website URL and/or address of choice
-#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
-#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
-#The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
 #The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 #The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
 #A friendly message thanking the customer and/or encouraging the customer to shop again
